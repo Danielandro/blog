db6906debaf92cd3d81dc3dcb6aca2edac5cc170
Daniel Ajilore <mtme13@hotmail.com>
2018-12-05 13:35:58 +0000

    add article resource - REST for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index' # map localhost:3000/welcome/index requests to welcome controller's index action
+
+  #use resources method to declare standard REST resource
+  resources :articles
+
   root 'welcome#index' # map '/' requests to welcome controller's index action
 end
