89beb151c35ff4e417c02bab852749ad13fe8e35
Daniel Ajilore <mtme13@hotmail.com>
2018-12-05 17:38:28 +0000

    save article in database via ArticlesController::create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def new
-
   end
 
   def create
@@ -8,6 +7,21 @@ class ArticlesController < ApplicationController
     # key = plain, value = params[:article].inspect
     # params method returns an ActionController::Parameters object
     # Can use this object to access keys of hash using strings or symbols (e.g. :article)
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+
+    # Article model initialized with respective attributes i.e. params[:article]
+    # title & text from :article are automatically mapped to their correct columns in the db
+    # @article = Article.new(params[:article]) - won't work due to Strong Parameters
+    @article = Article.new(article_params)
+    # save model in db. Boolean is returned
+    @article.save
+    # show the action???
+    redirect_to @article
+
+    private
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end      
+    
   end
 end
