efec0fffe2569e1a36b928243db52f81f7148a43
Daniel Ajilore <mtme13@hotmail.com>
2018-12-05 16:32:43 +0000

    add params note

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    # pass render a simple hash:
+    # key = plain, value = params[:article].inspect
+    # params method returns an ActionController::Parameters object
+    # Can use this object to access keys of hash using strings or symbols (e.g. :article)
     render plain: params[:article].inspect
   end
 end
