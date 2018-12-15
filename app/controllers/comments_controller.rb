b479355b406bfa628c595d11efe90ca33fe0a1a6
Daniel Ajilore <mtme13@hotmail.com>
2018-12-15 23:12:53 +0000

    add create action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    # keep track of article comment is related to  
+    @article = Article.find(params[:article_id])
+    # create + save comment. Links comment to article.
+    @comment = @article.comments.create(comment_params)
+    # redirect to show view
+    redirect_to article_path(@article)
+  end
+
+  private
+    # Comment cannot be empty. Accept commenter & body parameters only
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
